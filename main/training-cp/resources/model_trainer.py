912f397123b1291c97361e49dd644ee2d837c959
an-ahmad <70645719+an-ahmad@users.noreply.github.com>
2020-10-03 15:42:42 +0200

    Update model_trainer.py

--- main/training-cp/resources/model_trainer.py
+++ main/training-cp/resources/model_trainer.py
@@ -17,23 +17,19 @@ def train(dataset):
     X = dataset[:, 0:4]
     Y = dataset[:, 4]
     encoder = OneHotEncoder(sparse=False)
-	y = encoder.fit_transform(Y)
-	
-	#splits
-	train_x, test_x, train_y, test_y = train_test_split(X, y, test_size=0.20)
-	
-	model = Sequential()
-	model.add(Dense(10, input_shape=(4,), activation='relu', name='fc1'))
-	model.add(Dense(10, activation='relu', name='fc2'))
-	model.add(Dense(3, activation='softmax', name='output'))
-	# Adam optimizer with learning rate of 0.001
-	optimizer = Adam(lr=0.001)
-	model.compile(optimizer, loss='categorical_crossentropy', metrics=['accuracy'])
-   
+    y = encoder.fit_transform(Y)
+    #splits
+    train_x, test_x, train_y, test_y = train_test_split(X, y, test_size=0.20)
+    model = Sequential()
+    model.add(Dense(10, input_shape=(4,), activation='relu', name='fc1'))
+    model.add(Dense(10, activation='relu', name='fc2'))
+    model.add(Dense(3, activation='softmax', name='output'))
+    # Adam optimizer with learning rate of 0.001
+    optimizer = Adam(lr=0.001)
+    model.compile(optimizer, loss='categorical_crossentropy', metrics=['accuracy'])
     # Train the model
-	model.fit(train_x, train_y, verbose=2, batch_size=5, epochs=151)
-	
-	# evaluate the model
+    model.fit(train_x, train_y, verbose=2, batch_size=5, epochs=151)
+    # evaluate the model
     scores = model.evaluate(test_x, test_y)
     print(model.metrics_names)
     text_out = {
